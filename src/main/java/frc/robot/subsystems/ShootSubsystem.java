1887c0285269590f8e13d845d26e64f14cf62849
EnzoHippo <116667598+EnzoHippo@users.noreply.github.com>
2022-11-09 09:11:52 -0800

    Update ShootSubsystem.java

--- src/main/java/frc/robot/subsystems/ShootSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShootSubsystem.java
@@ -1,3 +1,19 @@
-public class ShootSubsystem {
-    
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase
+{
+    private final CANSparkMax m_shooter = new CANSparkMax(DriveConstants.shooterMotor, MotorType.kBrushless);
+    public ShooterSubsystem()
+    { 
+
+    }
+    public void shoot(double speed)
+    {
+        System.out.println(speed);
+        m_shooter.set(speed);
+    }
 }
